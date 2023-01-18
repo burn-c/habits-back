b89c47f69bb8c92cdc31952b8cfbbd3b60de7a77
Carlos Oliveira <carlos.burndev@gmail.com>
2023-01-18 00:01:04 -0300

    feat(routes): adds /habits POST

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,35 @@
+import dayjs from "dayjs"
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/all', async () => {
-    const habits = await prisma.habit.findMany()
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+  
+    const { title, weekDays } = createHabitBody.parse(request.body)
 
-    return habits
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        habitWeekDays: {
+          create: weekDays.map( weekDays => {
+            return {
+              week_day: weekDays
+            }
+          })
+        }
+      }
+    })
+
+    console.log(`➕ Create new habits`)
   })
 }
