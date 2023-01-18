fc56b71829ec4438370db37532018d65a0c10e3f
Carlos Oliveira <carlos.burndev@gmail.com>
2023-01-17 23:39:32 -0300

    refact: adds lib folder and routes file

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,16 @@
 /**
  * Método HTTP: Get, Post, Put, Patch e Delete
  */
-
-import { PrismaClient } from '@prisma/client'
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 
+import { appRoutes } from './routes'
+
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 
-app.get('/all', async () => {
-  const habits = await prisma.habit.findMany()
-
-	return habits
-})
+app.register(appRoutes)
 
 app.listen({
 	port: 3333
